64fcb5213711a5db6165f63cc153d903bd583c06
devabdo787 <abdomanok12@gmail.com>
2026-02-22 17:45:23 +0200

    Update auth.js

--- api/auth.js
+++ api/auth.js
@@ -2,6 +2,8 @@ const fetch = require('node-fetch');
 
 export default async function handler(req, res) {
     const { code } = req.query;
+    if (!code) return res.status(400).json({ hasRole: false });
+
     try {
         const tokenRes = await fetch('https://discord.com/api/oauth2/token', {
             method: 'POST',
@@ -16,14 +18,14 @@ export default async function handler(req, res) {
         });
         const tokenData = await tokenRes.json();
 
-        // البوت يطلب بياناتك من السيرفر باستخدام الـ Intents
         const memberRes = await fetch(`https://discord.com/api/guilds/${process.env.GUILD_ID}/members/${tokenData.user_id}`, {
             headers: { Authorization: `Bot ${process.env.BOT_TOKEN}` },
         });
         const memberData = await memberRes.json();
         
-        // التحقق هل رتبة الإدارة (ROLE_ID) موجودة في حسابك؟
         const hasRole = memberData.roles && memberData.roles.includes(process.env.ROLE_ID);
         res.status(200).json({ hasRole: !!hasRole });
-    } catch (err) { res.status(500).json({ hasRole: false }); }
+    } catch (err) {
+        res.status(500).json({ error: "Discord API Error" });
+    }
 }
